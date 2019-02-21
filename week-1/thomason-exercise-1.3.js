9eed1978eb7d005ad208f6f4cfe8a3b1ee1e9f98
Tyson Thomason <tthomason@mailserviceslc.com>
2019-02-21 12:51:04 -0600

    finished exercise 1.3

---
+++ week-1/thomason-exercise-1.3.js
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title:  Exercise 1.3
+; File Name: thomason-exercise-1.3.js
+; Author: William Thomason
+; Date:   19 February 2019
+; Description: Duck Typing, A.K.A “Interfaces”
+;===========================================
+*/
+
+//HEADER
+const header = require('../thomason-header.js');
+console.log(header.display("William", "Thomason", "Exercise 1.3") + "\n");
+
+var url = require('url');
+
+var parsedURL = url.parse('https://www.williamthomason.info/profile?name=tyson');
+
+console.log(parsedURL.protocol);
+console.log(parsedURL.host);
+console.log(parsedURL.query);
