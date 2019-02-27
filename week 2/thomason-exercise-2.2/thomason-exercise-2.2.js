366b50ccda67d6da18a7e075b4f38f6f726d7932
Tyson Thomason <tthomason@mailserviceslc.com>
2019-02-27 09:39:16 -0600

    finished exercise 2.2

---
+++ week 2/thomason-exercise-2.2/thomason-exercise-2.2.js
@@ -0,0 +1,35 @@
+/*
+============================================
+; Title:  Exercise 2.2
+; File Name: thomason-exercise-2.2.js
+; Author: William Thomason
+; Date:   27 February 2019
+; Description: Hello World
+;===========================================
+*/
+
+//HEADER
+const header = require('../thomason-header.js');
+console.log(header.display("William", "Thomason", "Exercise 2.2") + "\n");
+
+//Start of program
+
+//Variable declaration:
+var express = require('express');
+var http = require('http');
+
+
+var app = express();
+
+app.use(function(req, res){
+    console.log("In comes a request to: %s", req.url);
+
+    res.end("\n Hello World \n");
+});
+
+
+http.createServer(app).listen(8080, function(){
+    console.log('Application started on port %s', 8080);
+})
+
+//End of program
