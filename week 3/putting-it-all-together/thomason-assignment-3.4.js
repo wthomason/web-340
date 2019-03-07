40585b7392bac0cbfe37c5be8b59cdb188ddb5cd
Tyson Thomason <tthomason@mailserviceslc.com>
2019-03-07 14:24:34 -0600

    finished assignment 3.4

---
+++ week 3/putting-it-all-together/thomason-assignment-3.4.js
@@ -0,0 +1,83 @@
+/*
+============================================
+; Title:  Assignment 3.4
+; File Name: thomason-assignment-3.4.js
+; Author: William Thomason
+; Date:   7 March 2019
+; Description: Putting it all together
+;===========================================
+*/
+
+//HEADER
+const header = require('../../thomason-header.js');
+console.log(header.display("William", "Thomason", "Assignment 3.4") + "\n");
+
+//Start of program
+
+var express = require("express");
+
+var http = require("http");
+
+var path = require("path");
+
+var logger = require("morgan");
+
+
+var app = express();
+
+
+app.set("views", path.resolve(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
+
+app.use(logger("short"));
+
+
+app.get("/", function(request, response) {
+
+    response.render("index", {
+
+        message: "home page"
+
+    });
+
+});
+
+app.get("/about", function(request, response) {
+
+    response.render("about", {
+
+        message: "about page"
+
+    });
+
+});
+
+app.get("/contact", function(request, response) {
+
+    response.render("contact", {
+
+        message: "contact page"
+
+    })
+
+});
+
+app.get("/products", function(request, response) {
+
+   response.render("products", {
+
+       message: "products page"
+
+   });
+
+});
+
+http.createServer(app).listen(8080, function() {
+
+   console.log("Application started on port 8080.");
+
+});
+
+//END PROGRAM
