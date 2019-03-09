ff3733b558fe3a01a70ef881b763a03dccf45c39
wthomason <williamthomason92@yahoo.com>
2019-03-09 16:52:56 -0600

    finished assignment 3.4

--- week 3/putting-it-all-together/thomason-assignment-3.4.js
+++ week 3/putting-it-all-together/thomason-assignment-3.4.js
@@ -14,26 +14,22 @@ console.log(header.display("William", "Thomason", "Assignment 3.4") + "\n");
 
 //Start of program
 
+//Requireing all the dependencies
 var express = require("express");
-
 var http = require("http");
-
 var path = require("path");
-
 var logger = require("morgan");
-
 
 var app = express();
 
-
+//telling express where to find the template files
 app.set("views", path.resolve(__dirname, "views"));
-
 app.set("view engine", "ejs");
 
-
+//setting morgan logger with the "short" parameter
 app.use(logger("short"));
 
-
+//defining routes and sending the response of what ejs template file to use wit a message.
 app.get("/", function(request, response) {
 
     response.render("index", {
@@ -74,6 +70,8 @@ app.get("/products", function(request, response) {
 
 });
 
+
+//starting server on localhost:8080
 http.createServer(app).listen(8080, function() {
 
    console.log("Application started on port 8080.");
